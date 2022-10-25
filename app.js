bb36950e29f98f50ae1e27b75eee39c217d8eb29
Luke Dixon <lukedixon41@yahoo.com>
2022-10-24 18:39:21 -0600

    updtaed

--- app.js
+++ app.js
@@ -4,16 +4,41 @@ console.log(
 );
 
 // Exercise 1
-console.log("EXERCISE 1:\n==========\n");
+var a = 20;
+var b = 4;
+var add = a + b;
+var minus = a - b; 
+var multiply = a * b;
+var dividing = a / b;
+console.log(add, minus, multiply, dividing);
 
 // YOUR CODE HERE
 
 // Exercise 2
-console.log("EXERCISE 2:\n==========\n");
+let num = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Frodo";
+let lastName = "Baggins";
+console.log(num + str);
+console.log(num + str2);
+console.log( num + isPresent);
+console.log(firstName + num);
+console.log(isPresent + str);
+console.log(firstName + lastName);
 
 // YOUR CODE HERE
 
 // Exercise 3
-console.log("EXERCISE 3:\n==========\n");
+let val = 5;
+let str3 = "5";
+let str4 = "five";
+let isAwake = false;
+console.log(val == str3, val === str3, !isAwake);
+console.log("eleven" == str4 && val >= str3);
+console.log(!isAwake || isAwake);
+console.log(0 == false, 0 === false, 0 != false);
+console.log(0 !== false);
 
 // YOUR CODE HERE
